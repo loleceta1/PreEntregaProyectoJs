5c4b4166995f4f676955db42612280e81057343b
loreTarantelli <cecilialorenatarantelli@gmail.com>
2022-04-13 18:36:49 -0300

    agregando fetch

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,31 @@
-const productos = [
-    { id: 1, titulo: "Masajes Relajación", precio: 999, imagen: '../imagenesTARA/massage-therapy-1612308_1920.jpg' },
+let productos = [];
+fetch("../data.json")
+    .then((response) => response.json())
+    .then((data) => generarCards(data.productos))
+
+fetch("../data.json")
+    .then((response) => response.json())
+    .then((data) => productos = data.productos)
+
+    /*{ id: 1, titulo: "Masajes Relajación", precio: 999, imagen: '../imagenesTARA/massage-therapy-1612308_1920.jpg' },
     { id: 2, titulo: "Reiki", precio: 800, imagen: '../imagenesTARA/hands-1327811_1920.jpg' },
     { id: 3, titulo: "Tratamiento Facial", precio: 1500, imagen: '../imagenesTARA/woman-3141766_1920.jpg'},
-    { id: 4, titulo: "Tratamiento Corporal", precio: ' ',imagen: '../imagenesTARA/woman-567021_1920.jpg'},
-];
+    { id: 4, titulo: "Tratamiento Corporal", precio: ' ',imagen: '../imagenesTARA/woman-567021_1920.jpg'},*/
 
-const agregarAlCarrito = (idProducto) => {
+
+const agregarAlCarrito = (idProducto, cantidadAgregados = 1) => {
 
      /*const valorCantidad = document.getElementById(
         `cantidad- ${elementoDelArray.id}`
      ).value*/
-
+    
     const productoAgregado = productos.find(producto=> producto.id === idProducto);
 
-    carrito.push(productoAgregado);
+    const existeElProducto = productos.findIndex((producto) => producto.id === productoAgregado.id);
+    existeElProducto ? productos [existeElProducto].cantidad += cantidadAgregados : carrito.push(productoAgregado);
 
     localStorage.setItem("carrito", JSON.stringify(carrito));
+    //calcularTotalCarrito();
 
     Swal.fire({
         title: `Acabas de seleccionar: ${productoAgregado.titulo}`,
@@ -28,7 +38,15 @@ const agregarAlCarrito = (idProducto) => {
 
 };
 
-   
+const verDetalle = (id) => {
+    const existeElProducto = productos.findIndex((producto) =>producto.id === id);
+
+    localStorage.setItem("productoDetalles", JSON.stringify(productos[existeElProducto]));    
+    location.href = "/detalle.html";
+    };
+
+
+    
 generarCards(productos);
 
 function generarCards(productosAMostrar){
@@ -57,6 +75,9 @@ function generarCards(productosAMostrar){
                         class="btn btn-outline-dark mt-auto" href="Seleccion">
                         Seleccionar
                     </button>
+                    <button class="btn btn-outline-dark mt-auto"
+                     onclick="verDetalle(${elementoDelArray.id})"
+                     >Ver Info</button>
                 </div>
             </div>
         </div>
@@ -68,3 +89,20 @@ function generarCards(productosAMostrar){
 function mostrarCardsEnElHTML(cards) {
     document.getElementById("listado-servicios").innerHTML = cards;
 };
+
+
+const calcularTotalCarrito = () => {
+    const totalCarrito = carrito.reduce((accum, productos) => accum + (productos.precio * productos.cantidad), 0);
+
+    document.getElementById("cantidad-seleccionado").innerHTML = totalCarrito;
+}
+
+
+/*// Cart.html
+const sumarCantidad = (idProducto) => {
+    const indiceEncontradoProducto = carrito.findIndex((elemento) => {
+        return elemento.id === idProducto;
+    });
+    carrito[indiceEncontradoProducto].cantidad += 1;
+    // re-renderizar el html
+}*/ 
